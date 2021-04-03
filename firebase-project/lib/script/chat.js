2988242bf74b47f925825b337e20d01cded43d53
thanh1998 <thanhstone98@gmail.com>
2021-04-03 09:24:10 +0700

    Update project

--- firebase-project/lib/script/chat.js
+++ firebase-project/lib/script/chat.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   storageBucket: "my-test-project-be939.appspot.com",
   messagingSenderId: "139014992855",
   appId: "1:139014992855:web:bee272e4bab58b2754d192",
-  measurementId: "G-NLWDVCB465"
+  measurementId: "G-NLWDVCB465",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -14,3 +14,16 @@ const database = firebase.database();
 const root = "my-test-project-be939-default-rtdb/";
 const detail = "detail/";
 const thread = "thread/";
+$(document).ready(function () {
+  var ref = database.ref(thread);
+
+  ref.child("abc1").on(
+    "value",
+    (snapshot) => {
+   console.log(snapshot.val())
+    },
+    (error) => console.log(error),
+  );
+});
+//Lấy danh sách chat
+function getListChat(searchKey) {}
